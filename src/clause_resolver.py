102fa51175f247b4d781f80849ca37dcf91a861c
r0ckwav3 <peterrockwave@gmail.com>
2024-12-08 10:52:10 -0800

    remove unneccesary wires piped through clause resolver

--- src/clause_resolver.py
+++ src/clause_resolver.py
@@ -5,14 +5,11 @@ from helpers import wirevector_list
 
 # exposed wires:
 # Inputs:
-#  - clause_id_i:  id of clause to resolve
 #  - cs_vars_i:    array of variables ids in this clause (from storage)
 #  - cs_negated_i: array of variable negation bits for this clause
 #  - var_vals_i:     values of variables (from storage)
 #  - var_assigned_i: assigned bits of variables
 # Outputs:
-#  - cs_addr_o:       address sent to clause storage
-#  - va_addrs_o:      addresses sent to variable assignment storage
 #  - clause_status_o: returned status of the clause - 0:unknown, 1:unsat, 2: sat or 3: forced variable
 #  - implied_var_o:   implied variable (if status = 3)
 #  - implied_val_o:   implied value (if status = 3)
@@ -20,16 +17,12 @@ from helpers import wirevector_list
 class ClauseResolver:
     def __init__(self, clause_bits: int, var_bits:int, clause_size: int):
         ## IN ##
-        self.clause_id_i = WireVector(bitwidth = clause_bits, name = "clause_id_i")
         self.cs_vars_i =      wirevector_list(var_bits, "cs_vars_i", clause_size)
         self.cs_negated_i =   wirevector_list(1, "cs_negated_i", clause_size)
         self.var_vals_i =     wirevector_list(1, "var_vals_i", clause_size)
         self.var_assigned_i = wirevector_list(1, "var_assigned_i", clause_size)
 
         ## OUT ##
-        self.cs_addr_o = WireVector(bitwidth = clause_bits, name = "cs_addr_o")
-        self.va_addrs_o = wirevector_list(var_bits, "va_addrs_o", clause_size)
-
         self.clause_status_o = WireVector(bitwidth = 2, name = "clause_status_o")
         self.implied_var_o = WireVector(bitwidth = var_bits, name = "implied_var_o")
         self.implied_val_o = WireVector(bitwidth = 1, name = "implied_val_o")
@@ -51,13 +44,6 @@ class ClauseResolver:
         unassigned_neg = WireVector(bitwidth = 1, name = "unassigned_neg")
 
         ## LOGIC ##
-
-        # piping memory addresses around
-        self.cs_addr_o <<= self.clause_id_i
-        for i in range(clause_size):
-            self.va_addrs_o[i] <<= self.cs_vars_i[i]
-
-        # resolve logic
         for i in range(clause_size):
             atom_vals[i] <<= (self.var_vals_i[i] ^ self.cs_negated_i[i]) & self.var_assigned_i[i]
             # we consider 0 vars to be assigned for the purposes of implication
--- src/tests/clause_resolver_tests.py
+++ src/tests/clause_resolver_tests.py
@@ -13,27 +13,21 @@ from helpers import connect_wire_lists, wirevector_list
 
 def basic_setup():
     clause_resolver = ClauseResolver(8, 8, 4)
-    clause_resolver.clause_id_i <<= Input(bitwidth = 8, name = "clause_id")
 
     connect_wire_lists(clause_resolver.cs_vars_i,      wirevector_list(8, "cs_vars", 4, Input))
     connect_wire_lists(clause_resolver.cs_negated_i,   wirevector_list(1, "cs_negated", 4, Input))
     connect_wire_lists(clause_resolver.var_vals_i,     wirevector_list(1, "var_vals", 4, Input))
     connect_wire_lists(clause_resolver.var_assigned_i, wirevector_list(1, "var_assigned", 4, Input))
 
-    cs_addr = Output(bitwidth = 8, name = "cs_addr")
-    va_addrs = wirevector_list(8, "va_addrs", 4, Output)
     clause_status = Output(bitwidth = 2, name = "clause_status")
     implied_var = Output(bitwidth = 8, name = "implied_var")
     implied_val = Output(bitwidth = 1, name = "implied_val")
 
-    cs_addr <<= clause_resolver.cs_addr_o
-    connect_wire_lists(va_addrs, clause_resolver.va_addrs_o)
     clause_status <<= clause_resolver.clause_status_o
     implied_var <<= clause_resolver.implied_var_o
     implied_val <<= clause_resolver.implied_val_o
 
 DEFAULT_INPUT = {
-    "clause_id": 0,
     "cs_vars_0": 0x10,
     "cs_vars_1": 0x11,
     "cs_vars_2": 0x12,
@@ -51,31 +45,6 @@ DEFAULT_INPUT = {
     "var_assigned_2": 0,
     "var_assigned_3": 0
 }
-
-def clause_resolver_addr_test():
-    pyrtl.reset_working_block()
-    pyrtl.set_debug_mode(True)
-
-    # setup
-    basic_setup()
-
-    # test
-    sim_trace = pyrtl.SimulationTrace()
-    sim = pyrtl.Simulation(tracer=sim_trace)
-
-    inputs = copy.deepcopy(DEFAULT_INPUT)
-    inputs["clause_id"] = 0x67
-    inputs["cs_vars_0"] = 0x89
-    inputs["cs_vars_1"] = 0xab
-    inputs["cs_vars_2"] = 0xcd
-    inputs["cs_vars_3"] = 0xef
-    sim.step(inputs)
-
-    assert sim_trace.trace["cs_addr"][-1] == 0x67
-    assert sim_trace.trace["va_addrs_0"][-1] == 0x89
-    assert sim_trace.trace["va_addrs_1"][-1] == 0xab
-    assert sim_trace.trace["va_addrs_2"][-1] == 0xcd
-    assert sim_trace.trace["va_addrs_3"][-1] == 0xef
 
 def clause_resolver_sat_unsat_test():
     pyrtl.reset_working_block()
@@ -262,7 +231,6 @@ def clause_resolver_empty_test():
 
 
 tests = [
-    clause_resolver_addr_test,
     clause_resolver_sat_unsat_test,
     clause_resolver_unknown_test,
     clause_resolver_implied_test,
